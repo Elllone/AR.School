f0cb8b2dc963998f49df1d845e38abd26bc99ecf
elllone <elllone>
2024-02-16 20:05:11 +0300

    refactor: Разделние на компоненты

---
+++ components/FigureView/FigureViewDescription.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{ figure: Figure }>()
+</script>
+
+<template>
+  <div class="d-flex justify-center align-center">
+    <v-card color="white">
+      <v-card-title>
+        <p class="text-h5 font-weight-bold">{{ props.figure.name }}</p>
+      </v-card-title>
+      <v-divider />
+      <v-container>
+        <p class="text-h6 font-weight-bold">{{ props.figure.description }}</p>
+      </v-container>
+    </v-card>
+  </div>
+</template>
---
+++ components/FigureView/FigureViewImage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useDisplay } from 'vuetify'
+const props = defineProps<{ figure: Figure }>()
+const { mobile } = useDisplay()
+</script>
+<template>
+  <div class="d-flex justify-center align-center">
+    <img
+      :src="props.figure.image"
+      :style="{
+        viewTransitionName: props.figure.slug,
+        maxWidth: mobile ? '300px' : '600px',
+      }"
+      class="figure_img"
+    />
+  </div>
+</template>
+<style scoped>
+.figure_img {
+  width: 100%;
+  border-radius: 4px;
+}
+</style>
