95ca0e37ac45f6d42a1de11797ec213cd271020a
elllone <elllone>
2024-02-16 15:40:00 +0300

    fix: Поправил верстку

--- components/ArSchoolWelcome.vue
+++ components/ArSchoolWelcome.vue
@@ -1,13 +1,19 @@
 <template>
   <v-row class="align-center">
-    <v-col cols="12" md="6">
+    <v-col
+      cols="12"
+      md="6"
+      :style="{
+        zIndex: 18,
+      }"
+    >
       <p class="text-h2 font-weight-bold">
         Погрузись в мир геометрии с <span class="text-blue">AR.School</span>
       </p>
     </v-col>
     <v-col cols="12" md="6">
       <div class="image_welcome">
-        <v-img width="700" src="/images/welcomeImage.png" />
+        <v-img width="700" src="/images/welcomeImage.png" cover />
       </div>
     </v-col>
   </v-row>
