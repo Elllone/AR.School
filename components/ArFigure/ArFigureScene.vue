6c4b8b4174ebd349ea50c1b5327d856d7d3d5c8f
elllone <elllone>
2024-03-28 02:11:30 +0300

    edit: Вынес камеру в отдельный компонент

--- components/ArFigure/ArFigureScene.vue
+++ components/ArFigure/ArFigureScene.vue
@@ -1,63 +1,34 @@
 <script setup lang="ts">
 const props = defineProps<{ figure: Figure }>()
-const webCamera = ref<HTMLVideoElement>()
-const mediaStream = ref<MediaStream>()
-const isMediaDevicesSupported = computed(() =>
-  navigator.mediaDevices ? 'supported' : 'not supported'
-)
-
-async function mountWebCamera() {
-  if (isMediaDevicesSupported.value === 'supported') {
-    mediaStream.value = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: 'environment',
-      },
-      audio: false,
-    })
-    webCamera.value!.srcObject = mediaStream.value
-    webCamera.value!.play()
-  }
-}
-function unmountWebCamera() {
-  if (mediaStream.value) {
-    mediaStream.value.getTracks().forEach((track) => track.stop())
-  }
-}
-
-onMounted(() => {
-  mountWebCamera()
-})
-onUnmounted(() => {
-  unmountWebCamera()
-})
 </script>
 <template>
   <div class="wrapper">
-    <template v-if="!isMediaDevicesSupported">
-      <p>
-        {{ isMediaDevicesSupported }}
-      </p>
-    </template>
-    <template v-if="isMediaDevicesSupported">
-      <div class="camera_container">
-        <video ref="webCamera" class="webCamera" />
-      </div>
-    </template>
+    <div class="figure_container">
+      <!-- <vr-figure-webgl :figure="props.figure" /> -->
+    </div>
+    <div class="camera_container">
+      <ar-figure-web-camera />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100%;
   width: 100%;
-  overflow: hidden;
+  position: relative;
 }
-.webCamera {
+.figure_container {
+  position: absolute;
+  max-width: 720px;
+  z-index: 18;
+
   height: 100%;
   width: 100%;
 }
 .camera_container {
-  height: 100%;
-  width: 100%;
 }
 </style>
---
+++ components/ArFigure/ArFigureWebCamera.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const webCameraElement = ref<HTMLVideoElement>()
+const mediaStream = ref<MediaStream>()
+const isMediaDevicesSupported = computed(() =>
+  navigator.mediaDevices ? 'supported' : 'not supported'
+)
+
+async function mountWebCamera() {
+  if (isMediaDevicesSupported.value === 'supported') {
+    mediaStream.value = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment',
+        width: { ideal: 4096 },
+        height: { ideal: 2160 },
+      },
+      audio: false,
+    })
+    webCameraElement.value!.srcObject = mediaStream.value
+    webCameraElement.value!.play()
+  }
+}
+
+function unmountWebCamera() {
+  if (mediaStream.value) {
+    mediaStream.value.getTracks().forEach((track) => track.stop())
+  }
+}
+onMounted(() => {
+  mountWebCamera()
+})
+onUnmounted(() => {
+  unmountWebCamera()
+})
+</script>
+<template>
+  <div v-if="isMediaDevicesSupported === 'supported'">
+    <video ref="webCameraElement" class="webCamera" />
+  </div>
+  <div v-else>
+    <p class="text-h3"> Нет доступа к камере </p>
+  </div>
+</template>
