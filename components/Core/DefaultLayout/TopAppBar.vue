878d81fceb6b502bd2e17e032c28f5147ff3f2eb
elllone <elllone>
2024-04-02 20:54:54 +0300

    edit: Удалил строку поиска

--- components/Core/DefaultLayout/TopAppBar.vue
+++ components/Core/DefaultLayout/TopAppBar.vue
@@ -5,17 +5,6 @@
     </template>
     <v-container class="mx-auto d-flex align-center justify-center">
       <v-spacer />
-      <v-responsive max-width="196">
-        <v-text-field
-          prepend-inner-icon="mdi-magnify"
-          label="Поиск"
-          rounded="lg"
-          density="compact"
-          variant="solo-filled"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-responsive>
     </v-container>
   </v-app-bar>
 </template>
